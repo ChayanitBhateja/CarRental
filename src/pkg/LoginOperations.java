3e31ac4a68c5159b8b16264e928b107150d12210
chayanitbhateja <chakshu1998022@gmail.com>
2019-10-17 20:44:34 +0530

    Login page completed

--- src/pkg/LoginOperations.java
+++ src/pkg/LoginOperations.java
@@ -1,5 +1,7 @@
 package pkg;
 
+import java.util.List;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -7,6 +9,7 @@ import org.hibernate.boot.Metadata;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.query.Query;
 
 import exception.InvalidUserException;
 
@@ -20,8 +23,16 @@ public class LoginOperations {
 		factory = meta.getSessionFactoryBuilder().build(); 
 	}
 	public boolean validateUser(String username, String password) throws InvalidUserException{
-		// TODO Auto-generated method stub
-		return false;
+		session = factory.openSession();
+		tx = session.beginTransaction();
+		@SuppressWarnings("unchecked")
+		Query<String> query = session.createQuery("select user.password from User user where user.username=: u");
+		query.setParameter("u", username);
+		List<String> list = query.list();
+		if(list.get(0).equals(password))
+			return true;
+		else
+			return false;		
 	}	
 
 }
